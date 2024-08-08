87b841dcb7ff5dda37a67652e115615811f97667
Artyom Korkhov <artyom.korkhov@gmail.com>
2024-08-09 00:11:15 +0200

    Handling secrets and even better telemetry with tracing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use once_cell::sync::Lazy;
 use rand::{distributions::Alphanumeric, Rng};
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use zero2prod::{
@@ -119,15 +120,16 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to postgres");
     connection
         .execute(format!(r#"CREATE DATABASE {}"#, &config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
-    let pool = PgPool::connect(&config.connection_string())
+    let pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to create PgPool");
     sqlx::migrate!("./migrations")
